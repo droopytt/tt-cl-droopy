1ab9db36c576f01661e24e4c86c471245811ba42
droopytt <41344599+droopytt@users.noreply.github.com>
2025-01-05 13:07:54 +0000

    Increase goon scale slightly

--- toontown/suit/DistributedCashbotBossAI.py
+++ toontown/suit/DistributedCashbotBossAI.py
@@ -380,7 +380,7 @@ class DistributedCashbotBossAI(DistributedBossCogAI.DistributedBossCogAI, FSM.FS
             # a place to hold them.
             self.goons = []
 
-        self.scales = [0.5, 0.5, 0.5, 0.5, 0.62]
+        self.scales = [0.5, 0.5, 0.5, 0.5, 0.6525]
         return
 
     def __resetBattleThreeObjects(self):
