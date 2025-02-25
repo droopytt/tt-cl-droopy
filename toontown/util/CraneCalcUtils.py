8aa55ce40a229428440df0ea9ffcb2058eac2dd8
droopytt <41344599+droopytt@users.noreply.github.com>
2025-02-25 01:10:19 +0000

    Add script to calculate probabilities of CFO being stunned

---
+++ toontown/util/CraneCalcUtils.py
@@ -0,0 +1,32 @@
+import random
+
+def progress_value_cashbot_static(boss_damage, max_damage=500, from_value=0.5, to_value=1.5):
+    damage_ratio = float(boss_damage) / float(max_damage)
+    progress_value = from_value + (to_value - from_value) * min(damage_ratio, 1)
+    return progress_value
+
+def progress_random_value(boss_damage, from_value, to_value, radius=0.2):
+    t = progress_value_cashbot_static(boss_damage, from_value=0, to_value=1)
+    radius = radius * (1.0 - abs(t - 0.5) * 2.0)
+    t += radius * random.uniform(-1, 1)
+    t = max(min(t, 1.0), 0.0)
+    return from_value + (to_value - from_value) * t
+
+
+MAX_GOON_RANGE = 466
+MIN_GOON_RANGE = 452
+STUN_GOON_SCALE = 0.60
+ITERATIONS = 2_000_000
+
+def probability_of_stun_with_hp(hp_remaining):
+    values = list()
+    for i in range(ITERATIONS):
+        damage = 500 - hp_remaining
+        goon_scale = progress_random_value(damage, 0.5, 1.5)
+        if goon_scale >= STUN_GOON_SCALE:
+            values.append(goon_scale)
+    probability = "{:.2f}".format((len(values) / ITERATIONS) * 100)
+    print(f"Probability of stun goon as 5th goon given {hp_remaining} after first 4 goons health is ~{probability}%")
+
+for health in range (MIN_GOON_RANGE, MAX_GOON_RANGE+1):
+    probability_of_stun_with_hp(health)
