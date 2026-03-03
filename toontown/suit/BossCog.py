020201c17afee66d36e4a9c0d7e505b404fb5562
droopytt <41344599+droopytt@users.noreply.github.com>
2026-03-03 16:10:35 +0000

    Fix double stunning animation

--- toontown/suit/BossCog.py
+++ toontown/suit/BossCog.py
@@ -483,7 +483,6 @@ class BossCog(Avatar.Avatar):
         self.stars.reparentTo(self.neck)
         base.playSfx(self.birdsSfx, looping=1)
         self.dizzy = dizzy
-        self.doAnimate('hit', now=1)
 
     def getAngryActorInterval(self, animName, **kw):
         if self.happy:
