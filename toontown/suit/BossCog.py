7474c57bb14481797b61d40be96fa22026f6d973
droopytt <41344599+droopytt@users.noreply.github.com>
2026-03-08 22:44:02 +0000

    Flash red?

--- toontown/suit/BossCog.py
+++ toontown/suit/BossCog.py
@@ -483,6 +483,7 @@ class BossCog(Avatar.Avatar):
         self.stars.reparentTo(self.neck)
         base.playSfx(self.birdsSfx, looping=1)
         self.dizzy = dizzy
+        self.flashRed()
 
     def getAngryActorInterval(self, animName, **kw):
         if self.happy:
